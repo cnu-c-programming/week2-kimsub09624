01b72ef1540e0cf1b095bd63894f1a3f4fde3ffd
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-13 11:59:18 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,10 +1,10 @@
-  1 #include <stdio.h>
-  2
-  3 int main() {
-  4         printf("%10s %10s %10s\n", "name", "id", "value");
-  5         printf("%10s %010d %10.2f\n", "kim", 12, 4.12);
-  6         printf("%10s %010d %10.2f\n", "lee", 1922, 3.21);
-  7         printf("%10s %010d %10.2f\n", "park", 432, 1.0);
-  8
-  9         return 0;
- 10 }
+#include <stdio.h>
+
+int main() {
+  printf("%10s %10s %10s\n", "name", "id", "value");
+  printf("%10s %010d %10.2f\n", "kim", 12, 4.12);
+  printf("%10s %010d %10.2f\n", "lee", 1922, 3.21);
+  printf("%10s %010d %10.2f\n", "park", 432, 1.0);
+
+  return 0;
+}
