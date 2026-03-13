a4cee89ee56ff2adc274f3b29c8909f87645ae4b
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-13 13:39:06 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+	int x = 0;
+	scanf("%d", &x);
+	
+	int div = 2;
 
-    return 0;
+	while(div <= x/2) {
+		int rem = x % div;
+		if(rem == 0) {
+			printf("false");
+			return 0;
+		} else {
+			div++;
+		}
+	}
+	
+	printf("true");
+	return 0;
 }
 
