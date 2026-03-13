fa3c6cc0a1b1ed1bd14fd885c9010bae860bcd24
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-13 12:31:03 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
 
-    return 0;
+	printf("char: %lu\n", sizeof(char));
+	printf("short: %lu\n", sizeof(short));
+	printf("int: %lu\n", sizeof(int));
+	printf("unsigned int: %lu\n", sizeof(unsigned int));
+	printf("long: %lu\n", sizeof(long));
+	printf("long long: %lu\n", sizeof(long long));
+	printf("float: %lu\n", sizeof(float));
+
+
+	return 0;
 }
