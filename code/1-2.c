3feb264795e544f1a759564b44da5f528721e054
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-13 11:53:07 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
-int main()
-{
-
-
-    return 0;
-}
+  1 #include <stdio.h>
+  2
+  3 int main() {
+  4         printf("%10s %10s %10s\n", "name", "id", "value");
+  5         printf("%10s %010d %10.2f\n", "kim", 12, 4.12);
+  6         printf("%10s %010d %10.2f\n", "lee", 1922, 3.21);
+  7         printf("%10s %010d %10.2f\n", "park", 432, 1.0);
+  8
+  9         return 0;
+ 10 }
